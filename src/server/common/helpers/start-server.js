122ced89e5552247825283a315522ea2d7a1e12b
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-06 10:32:37 +0000

    Log error

--- src/server/common/helpers/start-server.js
+++ src/server/common/helpers/start-server.js
@@ -19,7 +19,7 @@ async function checkCdpUploaderConnectivity(logger) {
     })
     logger.info({ baseUrl, response: payload }, 'CDP Uploader is reachable')
   } catch (error) {
-    logger.warn(
+    logger.error(
       {
         baseUrl,
         healthUrl,
