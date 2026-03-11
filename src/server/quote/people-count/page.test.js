6c06bf8259995d02b9b35c9bef9a0b47855cb6d2
Jonny Wyatt <jon.wyatt@equalexperts.com>
2026-03-11 10:55:36 +0000

    Use real redis for integration tests

--- src/server/quote/people-count/page.test.js
+++ src/server/quote/people-count/page.test.js
@@ -4,13 +4,6 @@ import { setupTestServer } from '../../../test-utils/setup-test-server.js'
 import { loadPage } from '../../../test-utils/load-page.js'
 import { submitForm } from '../../../test-utils/submit-form.js'
 import { expectInputError } from '../../../test-utils/assertions.js'
-import {
-  getQuoteDataFromCache,
-  getValidationFlashFromCache,
-  saveValidationFlashToCache
-} from '../session-cache.js'
-
-vi.mock('../session-cache.js')
 
 const pageHeading =
   'What is the maximum number of people the development will serve?'
@@ -18,9 +11,7 @@ const pageHeading =
 describe('People count page', () => {
   const getServer = setupTestServer()
 
-  beforeEach(() => vi.mocked(getQuoteDataFromCache).mockReturnValue({}))
-
-  it('should render a page heading and title', async () => {
+  it('should render all page elements', async () => {
     const document = await loadPage({
       requestUrl: routePath,
       server: getServer()
@@ -31,103 +22,50 @@ describe('People count page', () => {
     expect(document.title).toBe(
       'What is the maximum number of people the development will serve? - Nature Restoration Fund - Gov.uk'
     )
-  })
-
-  it('should link back to development-types if housing was not selected', async () => {
-    vi.mocked(getQuoteDataFromCache).mockReturnValue({
-      developmentTypes: ['other-residential']
-    })
-    const document = await loadPage({
-      requestUrl: routePath,
-      server: getServer()
-    })
     expect(getByRole(document, 'link', { name: 'Back' })).toHaveAttribute(
       'href',
       '/quote/development-types'
     )
-  })
-
-  it('should link back to residential if housing was selected', async () => {
-    vi.mocked(getQuoteDataFromCache).mockReturnValue({
-      developmentTypes: ['housing']
-    })
-    const document = await loadPage({
-      requestUrl: routePath,
-      server: getServer()
-    })
-    expect(getByRole(document, 'link', { name: 'Back' })).toHaveAttribute(
-      'href',
-      '/quote/residential'
-    )
-  })
-
-  it('should include a CSRF token inside the form, to prevent CSRF attacks', async () => {
-    const document = await loadPage({
-      requestUrl: routePath,
-      server: getServer()
-    })
+    expect(getByLabelText(document, pageHeading)).toHaveValue(null)
     const csrfToken = document.querySelector('form input[name="csrfToken"]')
     expect(csrfToken).toBeInTheDocument()
   })
 
-  it('should redirect and save validation errors to cache, after an invalid form submission', async () => {
-    const { response } = await submitForm({
+  it("should remember the user's previously entered value", async () => {
+    const { cookie } = await submitForm({
+      requestUrl: routePath,
+      server: getServer(),
+      formData: { peopleCount: '42' }
+    })
+    const document = await loadPage({
+      requestUrl: routePath,
+      server: getServer(),
+      cookie
+    })
+    expect(getByLabelText(document, pageHeading)).toHaveValue(42)
+  })
+
+  it('should show a validation error, after an invalid form submission', async () => {
+    const { response, cookie } = await submitForm({
       requestUrl: routePath,
       server: getServer(),
       formData: {}
     })
     expect(response.statusCode).toBe(303)
     expect(response.headers.location).toBe(routePath)
-    expect(saveValidationFlashToCache.mock.calls[0][1]).toEqual({
-      formSubmitData: {},
-      validationErrors: {
-        messagesByFormField: {
-          peopleCount: {
-            field: ['peopleCount'],
-            href: '#peopleCount',
-            text: 'Enter the maximum number of people to continue'
-          }
-        },
-        summary: [
-          {
-            field: ['peopleCount'],
-            href: '#peopleCount',
-            text: 'Enter the maximum number of people to continue'
-          }
-        ]
-      }
-    })
-  })
-
-  it('should show a validation error if the page is viewed after an invalid form submission', async () => {
-    const errorMessage = 'Enter the maximum number of people to continue'
-    vi.mocked(getValidationFlashFromCache).mockReturnValue({
-      validationErrors: {
-        summary: [
-          {
-            href: '#peopleCount',
-            text: errorMessage,
-            field: ['peopleCount']
-          }
-        ],
-        messagesByFormField: {
-          peopleCount: {
-            href: '#peopleCount',
-            text: errorMessage,
-            field: ['peopleCount']
-          }
-        }
-      },
-      formSubmitData: {}
-    })
     const document = await loadPage({
       requestUrl: routePath,
-      server: getServer()
+      server: getServer(),
+      cookie
     })
-    expectInputError({ document, inputLabel: pageHeading, errorMessage })
+    expectInputError({
+      document,
+      inputLabel: pageHeading,
+      errorMessage: 'Enter the maximum number of people to continue'
+    })
   })
 
-  it('should redirect to the next page if the form is submitted with a valid value', async () => {
+  it('should redirect to the next page', async () => {
     const { response } = await submitForm({
       requestUrl: routePath,
       server: getServer(),
@@ -135,22 +73,5 @@ describe('People count page', () => {
     })
     expect(response.statusCode).toBe(303)
     expect(response.headers.location).toBe('/quote/email')
-  })
-
-  it("should not pre-fill the input if the user didn't previously enter a value", async () => {
-    const document = await loadPage({
-      requestUrl: routePath,
-      server: getServer()
-    })
-    expect(getByLabelText(document, pageHeading)).toHaveValue(null)
-  })
-
-  it('should remember a previously entered value', async () => {
-    vi.mocked(getQuoteDataFromCache).mockReturnValue({ peopleCount: 42 })
-    const document = await loadPage({
-      requestUrl: routePath,
-      server: getServer()
-    })
-    expect(getByLabelText(document, pageHeading)).toHaveValue(42)
   })
 })
--- src/server/quote/residential/get-next-page.js
+++ src/server/quote/residential/get-next-page.js
@@ -2,7 +2,7 @@ import { routePath as routePathPeopleCount } from '../people-count/routes.js'
 import { routePath as routePathEmail } from '../email/routes.js'
 
 export default function getNextPage(quoteData) {
-  if (quoteData.developmentTypes.includes('other-residential')) {
+  if (quoteData.developmentTypes?.includes('other-residential')) {
     return routePathPeopleCount
   }
   return routePathEmail
