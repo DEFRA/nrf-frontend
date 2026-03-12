1df8d63d2b7ae7e3eeb4da441823b8ef2e53fc31
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-12 12:05:17 +0000

    formValidation

--- src/server/quote/check-boundary-result/form-validation.js
+++ src/server/quote/check-boundary-result/form-validation.js
@@ -2,7 +2,7 @@ import joi from 'joi'
 
 const errorMessage = 'Select if the boundary is correct'
 
-export default function getFormSchema() {
+export default function formValidation() {
   return joi.object({
     boundaryCorrect: joi.string().valid('yes', 'no').required().messages({
       'any.required': errorMessage,
