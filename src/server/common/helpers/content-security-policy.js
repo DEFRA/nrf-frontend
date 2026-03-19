e94ae768fef4075070a92e56fd128e0f039eec5f
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-19 11:46:49 +0000

    Removed data: from fontSrc, connectSrc, imgSrc, and frameSrc

--- src/server/common/helpers/content-security-policy.js
+++ src/server/common/helpers/content-security-policy.js
@@ -13,16 +13,16 @@ const contentSecurityPolicy = {
     // Hash 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' is to support a GOV.UK frontend script bundled within Nunjucks macros
     // https://frontend.design-system.service.gov.uk/import-javascript/#if-our-inline-javascript-snippet-is-blocked-by-a-content-security-policy
     defaultSrc: ['self'],
-    fontSrc: ['self', 'data:'],
-    connectSrc: ['self', 'wss', 'data:'],
+    fontSrc: ['self'],
+    connectSrc: ['self', 'wss'],
     mediaSrc: ['self'],
     styleSrc: ['self'],
     scriptSrc: [
       'self',
       "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='"
     ],
-    imgSrc: ['self', 'data:'],
-    frameSrc: ['self', 'data:'],
+    imgSrc: ['self'],
+    frameSrc: ['self'],
     objectSrc: ['none'],
     frameAncestors: ['none'],
     formAction: ['self', ...(cdpUploaderUrl ? [cdpUploaderUrl] : [])],
