9f200738cf49f6b0a07a44f8c680be1920b8204e
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-31 18:48:36 +0100

    Fix navigation

--- src/server/quote/residential/get-next-page.js
+++ src/server/quote/residential/get-next-page.js
@@ -1,9 +1,9 @@
 import { routePath as routePathPeopleCount } from '../people-count/routes.js'
-import { routePath as routePathEmail } from '../email/routes.js'
+import { routePath as routePathWasteWater } from '../waste-water/routes.js'
 
 export default function getNextPage(quoteData) {
   if (quoteData.developmentTypes?.includes('other-residential')) {
     return routePathPeopleCount
   }
-  return routePathEmail
+  return routePathWasteWater
 }
