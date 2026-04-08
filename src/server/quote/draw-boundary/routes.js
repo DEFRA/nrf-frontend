cb533cdf9dfcb10cc311530a8482374d68f8f32f
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-04-08 17:07:46 +0100

    NRF2-418 Add draw-boundary/check API (#162)
    * draw-boundary/check api
    
    * status code
    
    * clean up
    
    * Fix intersectingEdps
    
    * geometry schema

--- src/server/quote/draw-boundary/routes.js
+++ src/server/quote/draw-boundary/routes.js
@@ -1,13 +1,44 @@
+import joi from 'joi'
+
 import { quoteController } from '../controller-get.js'
 import getViewModel from './get-view-model.js'
+import { checkBoundaryHandler } from './controller.js'
 
 const routeId = 'draw-boundary'
 export const routePath = '/quote/draw-boundary'
+export const checkPath = '/quote/draw-boundary/check'
 
 export default [
   {
     method: 'GET',
     path: routePath,
     ...quoteController({ routeId, getViewModel })
+  },
+  {
+    method: 'POST',
+    path: checkPath,
+    options: {
+      validate: {
+        payload: joi.object({
+          geometry: joi
+            .object({
+              type: joi.string().valid('Polygon').required(),
+              coordinates: joi
+                .array()
+                .min(1)
+                .items(
+                  joi
+                    .array()
+                    .min(4)
+                    .items(joi.array().length(2).items(joi.number()).required())
+                )
+                .required()
+            })
+            .unknown(true)
+            .required()
+        })
+      }
+    },
+    handler: checkBoundaryHandler
   }
 ]
