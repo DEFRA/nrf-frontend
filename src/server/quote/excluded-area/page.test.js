d862d3cb8ecd267d56b51cdb67d0c6077e60c67e
fabs-hyperact <fabien.saujot@hyperact.co.uk>
2026-07-23 15:31:20 +0100

    NRF2-896: add a new frontend page for the excluded area info page (#321)
    * add a new frontend page for the excluded area info page
    
    * improve back button path management
    
    * code-review feedback: default link needs to be boundary type page
    
    ---------
    
    Co-authored-by: matha31 <saujot31@gmail.com>

---
+++ src/server/quote/excluded-area/page.test.js
@@ -0,0 +1,28 @@
+import { getByRole } from '@testing-library/dom'
+import { routePath } from './routes.js'
+import { setupTestServer } from '../../../test-utils/setup-test-server.js'
+import { loadPage } from '../../../test-utils/load-page.js'
+import { withValidQuoteSession } from '../../../test-utils/with-valid-quote-session.js'
+
+describe('Excluded area page', () => {
+  const getServer = setupTestServer()
+  let sessionCookie
+
+  beforeEach(
+    async () => (sessionCookie = await withValidQuoteSession(getServer()))
+  )
+
+  it('should render a page heading, title and back link', async () => {
+    const document = await loadPage({
+      requestUrl: routePath,
+      server: getServer(),
+      cookie: sessionCookie
+    })
+    expect(getByRole(document, 'heading', { level: 1 })).toHaveTextContent(
+      'Development is within the excluded area of this Environmental Delivery Plan (EDP)'
+    )
+    expect(document.title).toBe(
+      'Development is within the excluded area of this Environmental Delivery Plan (EDP) - Nature restoration levy - GOV.UK'
+    )
+  })
+})
