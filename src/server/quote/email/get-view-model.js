707419ac96ec9eb9bb5fa02df013ef9b4504144c
matha31 <saujot31@gmail.com>
2026-07-27 08:12:38 +0100

    remove the refactoring aroud boundary back link path

--- src/server/quote/email/get-view-model.js
+++ src/server/quote/email/get-view-model.js
@@ -1,10 +1,27 @@
 import { getPageTitle } from '../../common/helpers/page-title.js'
-import getBoundaryTypePathFromQuote from '../helpers/get-boundary-type-path-from-quote/index.js'
+import { routePath as drawBoundaryPath } from '../draw-boundary/routes.js'
+import { routePath as uploadPreviewMapPath } from '../upload-preview-map/routes.js'
+import { routePath as boundaryTypePath } from '../boundary-type/routes.js'
+import { createLogger } from '../../common/helpers/logging/logger.js'
 
 export const title = 'Enter your email address'
+const logger = createLogger()
 
 export default function getViewModel(quoteData = {}) {
-  const backLinkPath = getBoundaryTypePathFromQuote(quoteData)
+  let backLinkPath = boundaryTypePath
+    /**
+     * TODO: store the list of possible boundary entry types in a single location and import it here, rather than hardcoding the values in this function.
+     */
+    if (quoteData.boundaryEntryType === 'draw') {
+      backLinkPath = drawBoundaryPath
+    } else if (quoteData.boundaryEntryType === 'upload') {
+      backLinkPath = uploadPreviewMapPath
+    } else {
+      logger.error(
+        { boundaryEntryType: quoteData.boundaryEntryType },
+        'boundaryEntryType is not recognised'
+      )
+    }
 
   return {
     pageTitle: getPageTitle(title),
