679470b43a1bf78cd59ec4b7b344f38e91e614eb
matha31 <saujot31@gmail.com>
2026-07-27 08:17:30 +0100

    fix space issue

--- src/server/quote/email/get-view-model.js
+++ src/server/quote/email/get-view-model.js
@@ -9,19 +9,19 @@ const logger = createLogger()
 
 export default function getViewModel(quoteData = {}) {
   let backLinkPath = boundaryTypePath
-    /**
-     * TODO: store the list of possible boundary entry types in a single location and import it here, rather than hardcoding the values in this function.
-     */
-    if (quoteData.boundaryEntryType === 'draw') {
-      backLinkPath = drawBoundaryPath
-    } else if (quoteData.boundaryEntryType === 'upload') {
-      backLinkPath = uploadPreviewMapPath
-    } else {
-      logger.error(
-        { boundaryEntryType: quoteData.boundaryEntryType },
-        'boundaryEntryType is not recognised'
-      )
-    }
+  /**
+   * TODO: store the list of possible boundary entry types in a single location and import it here, rather than hardcoding the values in this function.
+   */
+  if (quoteData.boundaryEntryType === 'draw') {
+    backLinkPath = drawBoundaryPath
+  } else if (quoteData.boundaryEntryType === 'upload') {
+    backLinkPath = uploadPreviewMapPath
+  } else {
+    logger.error(
+      { boundaryEntryType: quoteData.boundaryEntryType },
+      'boundaryEntryType is not recognised'
+    )
+  }
 
   return {
     pageTitle: getPageTitle(title),
