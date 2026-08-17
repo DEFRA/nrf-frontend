3b6748a94fd1d86d5e3e6d15dc6cf7d6ef906e73
Jon Wyatt <jon.wyatt@equalexperts.com>
2026-08-17 16:40:54 +0100

    Log out errors fetching defra ID well known URL

--- src/server/auth/get-oidc-config.js
+++ src/server/auth/get-oidc-config.js
@@ -1,5 +1,8 @@
 import Wreck from '@hapi/wreck'
 import { config } from '../../config/config.js'
+import { createLogger } from '../common/helpers/logging/logger.js'
+
+const logger = createLogger()
 
 /**
  * Fetches OpenID Connect configuration from the well-known endpoint
@@ -13,10 +16,13 @@ export async function getOidcConfig() {
       'DEFRA_ID_WELL_KNOWN_URL not configured. Please set this environment variable.'
     )
   }
-
-  const { payload } = await Wreck.get(wellKnownUrl, {
-    json: true
-  })
-
-  return payload
+  try {
+    const { payload } = await Wreck.get(wellKnownUrl, {
+      json: true
+    })
+    return payload
+  } catch (err) {
+    logger.error(err, `Error fetching defraId.wellKnownUrl ${wellKnownUrl}`)
+    throw err
+  }
 }
