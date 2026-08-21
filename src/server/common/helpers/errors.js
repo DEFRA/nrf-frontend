3e46971e257d200b4273b9c82ea3da634933ee04
matha31 <saujot31@gmail.com>
2026-08-21 09:33:24 +0100

    add new error pages

--- src/server/common/helpers/errors.js
+++ src/server/common/helpers/errors.js
@@ -1,5 +1,12 @@
 import { statusCodes } from '../constants/status-codes.js'
 
+const errorTemplates = {
+  [statusCodes.badRequest]: 'error/400',
+  [statusCodes.notFound]: 'error/404',
+  [statusCodes.internalServerError]: 'error/500',
+  [statusCodes.serviceUnavailable]: 'error/503'
+}
+
 function statusCodeMessage(statusCode) {
   switch (statusCode) {
     case statusCodes.notFound:
@@ -9,10 +16,37 @@ function statusCodeMessage(statusCode) {
     case statusCodes.unauthorized:
       return 'Unauthorized'
     case statusCodes.badRequest:
-      return 'Bad Request'
+      return 'Your details are incomplete'
+    case statusCodes.serviceUnavailable:
+      return 'Service unavailable'
+    case statusCodes.internalServerError:
+      return 'Sorry, there is a problem with the service'
     default:
       return 'Something went wrong'
   }
+}
+
+/**
+ * Renders the error page for a Boom response status. Dedicated pages exist
+ * for 400, 404 and 503; all other statuses use the generic error page.
+ * @param {object} options - error page options
+ * @param {number} options.statusCode - HTTP status of the Boom response
+ * @param {string} options.errorMessage - human-readable message for the status
+ * @param {object} options.h - Hapi response toolkit
+ * @returns {object} Hapi view response
+ */
+function renderErrorPage({ statusCode, errorMessage, h }) {
+  const template = errorTemplates[statusCode]
+
+  if (template) {
+    return h.view(template, { pageTitle: errorMessage, statusCode })
+  }
+
+  return h.view('error/index', {
+    pageTitle: errorMessage,
+    heading: errorMessage,
+    statusCode
+  })
 }
 
 export function catchAll(request, h) {
@@ -29,11 +63,5 @@ export function catchAll(request, h) {
     request.logger.error(response, errorMessage)
   }
 
-  return h
-    .view('error/index', {
-      pageTitle: errorMessage,
-      heading: statusCode,
-      message: errorMessage
-    })
-    .code(statusCode)
+  return renderErrorPage({ statusCode, errorMessage, h }).code(statusCode)
 }
