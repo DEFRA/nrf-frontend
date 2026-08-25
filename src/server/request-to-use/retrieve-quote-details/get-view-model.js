e29df62387377ae7b0b93dd99b82303f74f05bd7
fabs-hyperact <fabien.saujot@hyperact.co.uk>
2026-08-25 10:27:40 +0100

    fix content feedbacks (#402)
    Co-authored-by: matha31 <saujot31@gmail.com>

--- src/server/request-to-use/retrieve-quote-details/get-view-model.js
+++ src/server/request-to-use/retrieve-quote-details/get-view-model.js
@@ -1,7 +1,7 @@
 import { getPageTitle } from '../../common/helpers/page-title.js'
 
 const pageHeading =
-  'Nature Restoration Fund – retrieve your quote details for the Nature Restoration Fund levy'
+  'Nature restoration levy – retrieve your quote details for the nature restoration levy'
 const pageTitle = pageHeading
 
 export default function getViewModel() {
